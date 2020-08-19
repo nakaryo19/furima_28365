1f852e3a9e01172115c9189adbcbb5a4f0689a9b
nakaryo19 <ryoryoest19@gmail.com>
2020-08-19 19:43:59 +0900

    出品機能の表示

---
+++ app/models/item.rb
@@ -0,0 +1,15 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category 
+  belongs_to_active_hash :product
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
+
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+  validates :name,:text,:image,:category_id,:product_id,:delivery_id,:area_id,:days_id, presence: true
+  validates :price, presence: true, inclusion: 300..9999999
+end
