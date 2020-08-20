e8d8e95584138530d2d38cd15db2d510341fd2ce
nakaryo19 <ryoryoest19@gmail.com>
2020-08-20 21:29:02 +0900

    出品ページに価格変動導入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    context '空の場合のテスト'
+      it "画像なしは無効です" do
+       item = build(:item, image:[] )
+       item.valid?
+       expect(product.errors[:image]).to include("を入力してください")
+      end
+
+    it '名前なしは無効です' do
+      
+  
 end
