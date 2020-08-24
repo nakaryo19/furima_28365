9a7d2fb7ed96a28aac280b2d3118dbed217a13bd
nakaryo19 <ryoryoest19@gmail.com>
2020-08-24 11:23:29 +0900

    editディレクトリを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+      if item.update(item_params)
+        redirect_to item_path(item.id)
+      else
+        redirect_to action: :edit
+      end
+    else
+      redirect_to action: :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :text, :image, :category_id, :product_id, :delivery_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
